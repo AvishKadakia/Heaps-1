383ce26516f66babdcb9ef069c3cd148a1f55740
Avish Kadakia <avishkadakia1996@gmail.com>
2022-09-04 19:28:19 -0400

    Create Problem-1.py

---
+++ Problem-1.py
@@ -0,0 +1,21 @@
+
+# 720. Longest Word in Dictionary
+
+'''
+Leetcode all test cases passed: Yes
+Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+      n is length of nums
+      Time Complexity: O(n log k) 
+      Space Complexity: O(k)
+'''
+import heapq as hp
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        
+        for num in nums:
+            hp.heappush(heap,num)
+            if len(heap) > k:
+                hp.heappop(heap)
+        return heap[0]
